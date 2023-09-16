ae7b569bca8ec9fe2f6910f73d4398bcd10fac28
‘dingyl666’ <1811239843@qq.com>
2023-09-16 18:45:50 +0800

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,38 +1,38 @@
 import {request} from "./request";
 
-const baseUrl = 'http://localhost:8888' ;
+const BaseUrl = 'http://localhost:8888/api/v1' ;
 
 
 export const addUser = async (params:{ userId:number,name:string, }) => {
-    return await request(baseUrl + '/user/add', {
+    return await request(BaseUrl + '/user/add', {
         method: "POST",
         params: params
     })
 }
 
 export const delUser = async (userId:number) => {
-    return await request(baseUrl + '/user/del', {
+    return await request(BaseUrl + '/user/del', {
         method: "POST",
         params: {userId}
     })
 }
 
 export const upDataUser = async (params:{ userId:number,name:string, }) => {
-    return await request(baseUrl + '/user/up', {
+    return await request(BaseUrl + '/user/up', {
         method: "POST",
         params: params
     })
 }
 
 export const getUserList = async () => {
-    return await request(baseUrl + '/user/getList', {
+    return await request(BaseUrl + '/user/getList', {
         method: "GET",
         params: {}
     })
 }
 
 export const getUserId = async (userId:number) => {
-    return await request(baseUrl + '/user/getInfo', {
+    return await request(BaseUrl + '/user/getInfo', {
         method: "GET",
         params: {
             userId
