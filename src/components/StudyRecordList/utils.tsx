05ad0a0f14c0c5e9c2cd8573aa2611534818525c
‘dingyl666’ <1811239843@qq.com>
2023-09-16 18:49:03 +0800

    备注

--- src/components/StudyRecordList/utils.tsx
+++ src/components/StudyRecordList/utils.tsx
@@ -16,7 +16,7 @@ export class RecordList {
     }
 }
 export const recordList:RecordList[] = [
-    new RecordList(1,'/swagger','路由练习','模仿swagger做了一个类似的交互效果demo'),
+    new RecordList(1,'/swagger','路由锚点练习','模仿swagger的刷新重新定位做了一个类似的交互效果demo'),
     new RecordList(2,'/decorator','装饰器','ts装饰器深入学习'),
     new RecordList(3,'/nest/crud','nest练习','nest crud demo'),
 ]
